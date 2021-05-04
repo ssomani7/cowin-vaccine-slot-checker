1e1d1972fdc08852cb60f08662ab7a9a18e9c4c6
ssomani7 <ssomani@scu.edu>
2021-05-04 23:21:36 +0530

    commenting pytho virtual env settings for my local

--- cron_script.sh
+++ cron_script.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-export PATH=/usr/local/bin:$PATH
+# export PATH=/usr/local/bin:$PATH
 
 # This configuration is for my system. You might have to change this for your system to make sure that the slot_checker.py runs on python3
-export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
-export PROJECT_HOME=$HOME/Devel
-source /usr/local/bin/virtualenvwrapper.sh
+# export WORKON_HOME=$HOME/.virtualenvs
+# export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+# export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
+# export PROJECT_HOME=$HOME/Devel
+# source /usr/local/bin/virtualenvwrapper.sh
 
 # This has to be changed to the repository path on your system.
-cd <PATH>/cowin-vaccine-slot-checker/
+cd /Users/saurabhsomani/Documents/cowin-vaccine-slot-checker
 python3 slot_checker.py
